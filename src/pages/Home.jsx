0995a45e9cba54c9f869300cda1713ba4640552a
Alejandrolt <alejandrolt54@gmail.com>
2024-05-14 17:58:01 -0500

    taskreact08

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import { Container, Description } from './Home.styled';
+
+export const Home = () => {
+  return (
+    <>
+      <Container>
+        <Description>
+          <h1>HOME</h1>
+          <p>
+            The company itself is a very successful company. We can help him,
+            because the pain of the mind's problems here prevents them from
+            happening no matter what he seeks. At what moment did you choose
+            this question of mind? The client himself, the client, may pursue
+            the adipisation of the company. And the pain, and the flattery, that
+            repel him when it comes to choosing. Are some people forced to
+            suffer pain for what they don't, and the most deserving pains are
+            the guilt and, above all, not the benefits?
+          </p>
+        </Description>
+      </Container>
+    </>
+  );
+};
